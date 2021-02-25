1b2a55dc87f3c6322c303dbd533422767fa20acf
oxunjon <polatmuhammadumar@mail.ru>
2021-02-25 14:40:30 +0500

    foiz ishlatish

--- frontend/views/site/tadbir.php
+++ frontend/views/site/tadbir.php
@@ -40,153 +40,159 @@ data-open="click" data-menu="vertical-menu-modern" data-col="2-columns">
 <!-- projects table with monthly chart -->
 <div class="row">
 
-    <div class="col-xl-12 col-lg-12">
-        <div class="card">
-            <div class="card-header">
-                <h4 class="card-title">ИЖРО НАЗОРАТИ</h4>
-                <a class="heading-elements-toggle"><i class="ft-more-horizontal font-medium-3"></i></a>
-                <div class="heading-elements">
-                    <ul class="list-inline mb-0">
-                        <li><a data-action="reload"><i class="ft-rotate-cw"></i></a></li>
-                        <li><a data-action="expand"><i class="ft-maximize"></i></a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="card-content">
-                <div class="card-body">
-                    <!-- <p class="m-0">Total ongoing projects 6<span class="float-right"><a href="project-summary.html" target="_blank">Project Summary <i class="ft-arrow-right"></i></a></span></p> -->
-                </div>
-                <div class="table-responsive">
-                    <table class="table table-hover mb-10">
-                        <thead>
-                            <tr>
-                                <th class="width-300">Бошқармалар,Бўлимлар,Тизим ташкилотлари</th>
-                                
-                                <!-- <th>Statusi</th> -->
-                                <th class="width-350 text-center">Фоизда</th>
-                                <th class="width-350 text-center">Тадбир сони</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                          <?php foreach ($company as $key => $value) :?>
+  <div class="col-xl-12 col-lg-12">
+    <div class="card">
+      <div class="card-header">
+        <h4 class="card-title">ИЖРО НАЗОРАТИ</h4>
+        <a class="heading-elements-toggle"><i class="ft-more-horizontal font-medium-3"></i></a>
+        <div class="heading-elements">
+          <ul class="list-inline mb-0">
+            <li><a data-action="reload"><i class="ft-rotate-cw"></i></a></li>
+            <li><a data-action="expand"><i class="ft-maximize"></i></a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="card-content">
+        <div class="card-body">
+          <!-- <p class="m-0">Total ongoing projects 6<span class="float-right"><a href="project-summary.html" target="_blank">Project Summary <i class="ft-arrow-right"></i></a></span></p> -->
+        </div>
+        <div class="table-responsive">
+          <table class="table table-hover mb-10">
+            <thead>
+              <tr>
+                <th class="width-300">Бошқармалар,Бўлимлар,Тизим ташкилотлари</th>
 
-                            <tr>
-                                <td class="width-350">
-<?php $id = $value->id; ?>
-                                  <?=$value->company_name?></td>
-                                  <?php $count = count(common\models\Tadbir::find()->where(['company_id'=>$value->id])->all());
-                                  $calculate = count(common\models\Tadbir::find()->where(['company_id'=>$value->id,'tadbir_status'=>'bajarildi'])->all());
-                                  if ($count===0) {
-                                    $count = 1;
-                                  }
-                                  if ($calculate===0) {
-                                    $calculate = 1;
-                                  }
+                <!-- <th>Statusi</th> -->
+                <th class="width-350 text-center">Фоизда</th>
+                <th class="width-350 text-center">Тадбир сони</th>
+              </tr>
+            </thead>
+            <tbody>
+              <?php foreach ($company as $key => $value) :?>
+
+                <tr>
+                  <td class="width-350">
+                    <?php $id = $value->id; ?>
+                    <?=$value->company_name?></td>
+                    <?php $count = count(common\models\Tadbir::find()->where(['company_id'=>$value->id])->all());
+                    $calculate = count(common\models\Tadbir::find()->where(['company_id'=>$value->id,'tadbir_status'=>'bajarildi'])->all());
+                                  // echo $calculate;
+
 
 // echo $calculate;
-                                    $result = round(($calculate/$count)*100 , 2);
+
                                     // echo $result;
-                                  ?>
-                                  <td class="valign-middle">
-                                     <a href="<?=Url::to(['site/company','id' =>$id]) ?>" title=""><div class="progress m-0" style="height: 50px;">
-                                        <div class="progress-bar bg-success" role="progressbar" style="width: <?=$result ?>%" aria-valuenow="<?=$result ?>" aria-valuemin="0" aria-valuemax="100"><h3><?=$result ?>%</h3></div>
-                                    </div></a>
-                                </td>
-                                <td class="text-center"> 
-                                    <?= $count; ?>
-                                </td>
-                            </tr>
-                        <?php endforeach;?>
+                    if ($calculate===0) {
+                          $calculate = 1;
+                        }
+                        if ($count===0) {
+                          $count = 1;
+                          $result = '0';
+                        }else{
+                          $result = round(($calculate/$count)*100 , 2);
+                        }
+                    ?>
+                    <td class="valign-middle">
+                     <a href="<?=Url::to(['site/company','id' =>$id]) ?>" title=""><div class="progress m-0" style="height: 50px;">
+                      <div class="progress-bar bg-success" role="progressbar" style="width: <?=$result ?>%" aria-valuenow="<?=$result ?>" aria-valuemin="0" aria-valuemax="100"><h3>
+                        <?= $result ?>%</h3></div>
+                      </div></a>
+                    </td>
+                    <td class="text-center"> 
+                      <?= $count; ?>
+                    </td>
+                  </tr>
+                <?php endforeach;?>
 
-                    </tbody>
-                </table>
-            </div>
+              </tbody>
+            </table>
+          </div>
         </div>
-    </div>
-    <!-- Project Overview -->
-    <?php foreach ($company as $key => $com) : ?>
-     <!-- UI components start -->
-     <div class="row" id="ui">
+      </div>
+      <!-- Project Overview -->
+      <?php foreach ($company as $key => $com) : ?>
+       <!-- UI components start -->
+       <div class="row" id="ui">
         <div class="col-12">
-            <div class="card">
-                <div class="card-header">
-<h4 class="card-title text-center"><?=$com->company_name ?>ни жорий қилиш ва ривожлантириш <br>бошқармасининг 2021 йил март-май ойлари учун ЙЎЛ ХАРИТАСИ</h4>
-                    <a class="heading-elements-toggle"><i class="fa fa-ellipsis-v font-medium-3"></i></a>
-                    <div class="heading-elements">
-                        <ul class="list-inline mb-0">
-                            <li><a data-action="collapse"><i class="ft-minus"></i></a></li>
-                            <li><a data-action="reload"><i class="ft-rotate-cw"></i></a></li>
-                            <li><a data-action="expand"><i class="ft-maximize"></i></a></li>
-                            <li><a data-action="close"><i class="ft-x"></i></a></li>
-                        </ul>
-                    </div>
-                </div>
-                <?php
-                $tadbirlar = common\models\Tadbir::find()->where(['company_id'=>$com->id])->all(); ?>
-                <div class="card-content collapse show">
-                    <div class="table-responsive">
-                        <table class="table table-responsive table-bordered mb-10">
-                         <thead>
-                            <tr>
-                                <th>Тадбир номи</th>
-                                <th>Амалга ошириш механизми</th>
-                                <th>Эришиладиган натижа ва кўрсаткичлар</th>
-                                <th>Масуъллар</th>
-                                <th>Муддати</th>
-                                <th>Изох</th>
-                                <th>Ижро ҳолати</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php foreach ($tadbirlar as $key => $tadbir) : ?>
-                                <tr>
-
-                                    <td class="width-300 height-300 ">
-
-                                      <?=$tadbir->tadbir_name ?>
-                                  </td>
-                                  <td class="width-300 height-300 "> 
-                                     <?=$tadbir->tadbir_content ?>
-                                 </td>
-                                 <td class="width-300 height-300 ">
-                                     <?=$tadbir->tadbir_description ?>
-                                 </td>
-                                  <td class="width-300 height-300 ">
-                                     <?=$tadbir->masullar ?>
-                                 </td>
-                                 <td class="width-50"><?=Yii::$app->formatter->format($tadbir->tadbir_date, 'date');  ?></td>
-                                 <td class="width-100"><?=$tadbir->tadbir_result ?></td>
-
-                                 <td class="width-100">
-                                  <?php if($tadbir->tadbir_status==="bajarildi"): ?>
-                                    <button value="бажарилди" type="button" class="btn btn-success mr-1 mb-1">бажарилди</button>
-                                    <?php elseif($tadbir->tadbir_status==="bajarilmadi"): ?>
-                                    <button value="бажарилмади" type="button" class="btn btn-danger mr-1 mb-1">Муддати ўтиб ижро қилинган</button>
-                                    <?php elseif($tadbir->tadbir_status==="ijroda"): ?>
-                                    <button value="иш жараёнида" type="button" class="btn btn-secondary mr-1 mb-1">иш жараёнида</button>
-                                  <?php elseif($tadbir->tadbir_status==="ijroda"): ?>
-                                  <button value="ижрода" type="button" class="btn btn-warning mr-1 mb-1">ижрода</button>
-                                  <?php endif; ?>
-                                </td>
-                                
-                            </tr>
-                        <?php endforeach; ?>
-                    </tbody>
-                </table>
+          <div class="card">
+            <div class="card-header">
+              <h4 class="card-title text-center"><?=$com->company_name ?>ни жорий қилиш ва ривожлантириш <br>бошқармасининг 2021 йил март-май ойлари учун ЙЎЛ ХАРИТАСИ</h4>
+              <a class="heading-elements-toggle"><i class="fa fa-ellipsis-v font-medium-3"></i></a>
+              <div class="heading-elements">
+                <ul class="list-inline mb-0">
+                  <li><a data-action="collapse"><i class="ft-minus"></i></a></li>
+                  <li><a data-action="reload"><i class="ft-rotate-cw"></i></a></li>
+                  <li><a data-action="expand"><i class="ft-maximize"></i></a></li>
+                  <li><a data-action="close"><i class="ft-x"></i></a></li>
+                </ul>
+              </div>
             </div>
+            <?php
+            $tadbirlar = common\models\Tadbir::find()->where(['company_id'=>$com->id])->all(); ?>
+            <div class="card-content collapse show">
+              <div class="table-responsive">
+                <table class="table table-responsive table-bordered mb-10">
+                 <thead>
+                  <tr>
+                    <th>Тадбир номи</th>
+                    <th>Амалга ошириш механизми</th>
+                    <th>Эришиладиган натижа ва кўрсаткичлар</th>
+                    <th>Масуъллар</th>
+                    <th>Муддати</th>
+                    <th>Изох</th>
+                    <th>Ижро ҳолати</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <?php foreach ($tadbirlar as $key => $tadbir) : ?>
+                    <tr>
+
+                      <td class="width-300 height-300 ">
+
+                        <?=$tadbir->tadbir_name ?>
+                      </td>
+                      <td class="width-300 height-300 "> 
+                       <?=$tadbir->tadbir_content ?>
+                     </td>
+                     <td class="width-300 height-300 ">
+                       <?=$tadbir->tadbir_description ?>
+                     </td>
+                     <td class="width-300 height-300 ">
+                       <?=$tadbir->masullar ?>
+                     </td>
+                     <td class="width-50"><?=Yii::$app->formatter->format($tadbir->tadbir_date, 'date');  ?></td>
+                     <td class="width-100"><?=$tadbir->tadbir_result ?></td>
+
+                     <td class="width-100">
+                      <?php if($tadbir->tadbir_status==="bajarildi"): ?>
+                        <button value="бажарилди" type="button" class="btn btn-success mr-1 mb-1">бажарилди</button>
+                        <?php elseif($tadbir->tadbir_status==="bajarilmadi"): ?>
+                          <button value="бажарилмади" type="button" class="btn btn-danger mr-1 mb-1">Муддати ўтиб ижро қилинган</button>
+                          <?php elseif($tadbir->tadbir_status==="ijroda"): ?>
+                            <button value="иш жараёнида" type="button" class="btn btn-secondary mr-1 mb-1">иш жараёнида</button>
+                            <?php elseif($tadbir->tadbir_status==="ijroda"): ?>
+                              <button value="ижрода" type="button" class="btn btn-warning mr-1 mb-1">ижрода</button>
+                            <?php endif; ?>
+                          </td>
+
+                        </tr>
+                      <?php endforeach; ?>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
+        <!-- UI components end -->
+      <?php endforeach;?>
+      <!--/ Project Overview -->
     </div>
-</div>
-</div>
-<!-- UI components end -->
-<?php endforeach;?>
-<!--/ Project Overview -->
-</div>
 
 
 
-<?php $this->endBody() ?>
-</body>
-</html>
-<?php $this->endPage() ?>
+    <?php $this->endBody() ?>
+  </body>
+  </html>
+  <?php $this->endPage() ?>
 
