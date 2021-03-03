ee4fb9edbf5d45f3a07675fc914df91023f5c0ed
oxunjon <polatmuhammadumar@mail.ru>
2021-03-03 11:08:34 +0500

    adnjsndkasndjkasnkjda

--- backend/views/tadbir/index.php
+++ backend/views/tadbir/index.php
@@ -21,7 +21,7 @@ $this->params['breadcrumbs'][] = $this->title;
         <?= Html::a('Тадбир қушиш', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
 
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+    <?php if (user()->identity->role->name=="admin") :?>
 
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
@@ -92,11 +92,13 @@ $this->params['breadcrumbs'][] = $this->title;
             ['class' => 'yii\grid\ActionColumn',
           'template' => '{update}{view}{delete}',
         'buttons' => [
-            'delete' => function ($url, $model) {
+            'delete' => function ($url, $model) 
+            {
                 return Html::a(
                     '<span class="fa fa-trash" style="font-size:14px;padding:10px;"></span>', ['delete', 'id' => $model->id],
                     [
-                        'data' => [
+                        'data' => 
+                        [
                             'method' => 'POST',
                             'confirm' => 'Вы уверены, что хотите удалить этот элемент?',
                             'pjax' => false
@@ -106,27 +108,87 @@ $this->params['breadcrumbs'][] = $this->title;
                         'style' => 'display: contents'
                     ]);
             },
-            'view' => function ($url, $model) {
-                return Html::a('<span class="fa fa-eye" style="font-size:14px;"></span>', ['view', 'id' => $model->id], ['title' => 'Просмотр', 'aria-label' => 'Просмотр']);
+            'view' => function ($url, $model) 
+            {
+                return Html::a('<span class="fa fa-eye" style="font-size:14px;"></span>', 
+                    ['view', 'id' => $model->id], 
+                    ['title' => 'Просмотр', 'aria-label' => 'Просмотр']);
             }
-            // 'update' => function ($url, $model) {
-            //     return Html::a(
-            //         '<span class="fa fa-pencil-square-o" style="font-size:14px;"></span>', 'javascript:void(0)',
-            //         ['update',
-            //             'data' => [
-            //                 'id' => $model->id,
-            //             ],
-            //             'title' => 'O\'zgaritirish',
-            //             'aria-label' => 'O\'zgaritirish',
-            //             'class' => 'update-tadbir',
-            //             'style' => 'display: contents'
-            //         ]);
-            // }
         ],
     ],
 
         ],
     ]); ?>
+<?php else :?>
+<?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
 
+            // 'id',
+            // 'company_id',
+    [
+        'attribute' => 'company_id',
+        'format' => 'raw',
+        'filter' => ArrayHelper::map(Company::find()->all(), 'id', 'company_name'),
+        'filterType' => GridView::FILTER_SELECT2,
+        'filterWidgetOptions' =>
+        [
+            'size' => Select2::SIZE_SMALL,
+            'options' => ['prompt' => 'Выберите'],
+            'pluginOptions' => ['allowClear' => true],
+        ],
+        'value'=>'company.company_name'
+    ],
+            // 'tadbir_name:ntext',
+    [
+        'attribute' => 'tadbir_name',
+        'format' => 'html',
+        'filterInputOptions'=>['class' => 'form-control  input-sm'],
+    ],
+            // 'tadbir_content:ntext',
+            [
+        'attribute' => 'tadbir_content',
+        'format' => 'html',
+        'filterInputOptions'=>['class' => 'form-control  input-sm'],
+    ],
+            // 'tadbir_result:ntext',
+            [
+        'attribute' => 'tadbir_result',
+        'format' => 'html',
+        'filterInputOptions'=>['class' => 'form-control  input-sm'],
+    ],
+            [
+        'attribute' => 'masullar',
+        'format' => 'html',
+        'filterInputOptions'=>['class' => 'form-control  input-sm'],
+    ],
+            [
+        'attribute' => 'tadbir_status',
+        'format' => 'html',
+        'filterInputOptions'=>['class' => 'form-control  input-sm'],
+    ],
+    
+            // 'tadbir_date',
+            [
+        'attribute' => 'tadbir_date',
+        'format' => 'text',
+        'filterInputOptions'=>['class' => 'form-control  input-sm'],
+    ],
+            // 'tadbir_description:ntext',
+    [
+        'attribute' => 'tadbir_description',
+        'format' => 'html',
+        'filterInputOptions'=>['class' => 'form-control  input-sm'],
+    ],
 
+            
+            
+
+            ['class' => 'yii\grid\ActionColumn'],
+
+        ],
+    ]); ?>
+<?php endif; ?>
 </div>
